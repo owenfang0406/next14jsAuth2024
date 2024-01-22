6b50f46ec52ac680df55bb33989d6f88fb9abb7a
Owen Fang <owen.fang@technine.io>
2024-01-22 10:31:11 +0800

    testing ssh key on the other PC

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image"
-import { Button } from "@/components/ui/button"
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <Button variant="outline" className="font-semibold tex">
-      Hello world
+      Hello world!
     </Button>
-  )
+  );
 }
