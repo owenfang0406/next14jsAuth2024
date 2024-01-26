a1ac00082df042688e5f4e8a7435adbeaf1d87ba
Owen Fang <owen.fang@technine.io>
2024-01-26 18:32:40 +0800

    about to create login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,29 @@
+"use client";
+
 import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "../ui/form";
+import * as z from "zod";
+import { loginSchema } from "@/schemas";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="welcome back"
