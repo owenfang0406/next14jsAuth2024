480957d56d523087162ce51ce377dd443bdc8e39
FangWeiXiang <fangweixiang@FangWeiXiangdeMacBook-Air.local>
2024-01-30 01:08:47 +0800

    starting to build login form UI

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,8 @@
-"use client";
+"use client"
 
-import { CardWrapper } from "./card-wrapper";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { CardWrapper } from "./card-wrapper"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 import {
   Form,
   FormItem,
@@ -11,9 +11,11 @@ import {
   FormDescription,
   FormMessage,
   FormField,
-} from "../ui/form";
-import * as z from "zod";
-import { loginSchema } from "@/schemas";
+} from "../ui/form"
+import * as z from "zod"
+import { loginSchema } from "@/schemas"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
 
 export const LoginForm = () => {
   const form = useForm<z.infer<typeof loginSchema>>({
@@ -22,7 +24,7 @@ export const LoginForm = () => {
       email: "",
       password: "",
     },
-  });
+  })
 
   return (
     <CardWrapper
@@ -31,7 +33,45 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      LoginForm
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="example123@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="*******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
-  );
-};
+  )
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,10 @@
-import * as z from "zod";
+import * as z from "zod"
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
+  email: z.string().email({
+    message: "Email is required!",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required!",
+  }),
+})
